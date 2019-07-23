5a28246e647e348433efb1a8da4afe2cef6cb392
ianertson <sebbekarlsson97@gmail.com>
2019-07-23 19:01:29 +0200

    cleaned up bin_utils

--- src/bin_utils.c
+++ src/bin_utils.c
@@ -3,25 +3,19 @@
 
 int execute_binary(const char* filename, const char* binary_name)
 {
-    //int X = open("./output_file.txt", O_WRONLY);
-
     pid_t fk = fork();
-    if (!fk) { /* in child */
-            //dup2(X, 1); /* fd 1 is standard output,
-             //           so this makes standard out refer to the same file as X  */
-   //             close(X);
-
-                /* I'm using execl here rather than exec because
-                     *        it's easier to type the arguments. */
-                    execl(filename, binary_name, NULL);
-                        _exit(127); /* should not get here */
-    } else if (fk == -1) {
-            /* An error happened and you should do something about it. */
-            perror("fork"); /* print an error message */
+    if (!fk)
+    {
+        execl(filename, binary_name, NULL);
     }
-    //close(X); /* The parent doesn't need this anymore */
+    else if (fk == -1)
+    {
+        perror("fork"); 
+    }
+
     int status;
-    wait(&status); //simplest one, man wait for others
+    wait(&status);
     printf("child pid was %d, it exited with %d\n", fk, status);
-    exit(0);
+
+    return status;
 }
